94466e426f8fcb25b0699c40b7a4acab8831d03e
JackStuart <jack@jstuart.com.au>
2025-01-10 02:49:07 +0800

    Updating Parameters (#162)
    * Parameters were updated to starting at 0

--- scripts/PowerShell/Modules/AzSubscriptionManagement.psm1
+++ scripts/PowerShell/Modules/AzSubscriptionManagement.psm1
@@ -7,9 +7,9 @@
 Function Set-AzContextWrapper {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory, Position = 1)]
+        [Parameter(Mandatory, Position = 0)]
         [string]$SubscriptionId,
-        [Parameter(Position = 2)]
+        [Parameter(Position = 1)]
         [string]$Environment = 'AzureCloud'
     )
 
@@ -56,9 +56,9 @@ Function Set-AzContextWrapper {
 Function Register-AzProviderFeatureWrapper {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, Position = 1)]
+        [Parameter(Mandatory, Position = 0)]
         [string]$ProviderNamespace,
-        [Parameter(Mandatory, Position = 2)]
+        [Parameter(Mandatory, Position = 1)]
         [string]$FeatureName
     )
 
@@ -115,7 +115,7 @@ Function Register-AzProviderFeatureWrapper {
 Function Register-AzResourceProviderWrapper {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, Position = 1)]
+        [Parameter(Mandatory, Position = 0)]
         [string]$ProviderNamespace
     )
 
